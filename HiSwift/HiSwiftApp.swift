c8364e44b457c68583aaf918dcbe03586762eaf9
rinetrous <rinellie1228@icloud.com>
2021-03-30 13:39:51 +0900

    Initial Commit

---
+++ HiSwift/HiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiSwiftApp.swift
+//  HiSwift
+//
+//  Created by 野田凜太郎 on 2021/03/30.
+//
+
+import SwiftUI
+
+@main
+struct HiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
